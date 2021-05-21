bc7b829f208032361b48f48b784baaf5fb93d1b9
Szymon <piersiak.szymon@gmail.com>
2021-05-21 14:52:25 +0200

    preparation of the algorithm
    The algorithm checks how many times the ball bounces on the given minimum height

---
+++ ZadanieTestowe/ZadanieTestowe/Algorithm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieTestowe
+{
+    public class Algorithm
+    {
+        private double h { get; set; }
+        private double b { get; set; }
+        private double w { get; set; }
+        private int result { get; set; }
+
+        public Algorithm(double h, double b, double w)
+        {
+            this.h = h;
+            this.b = b;
+            this.w = w;
+
+            if (b == 1)
+                result = -1;
+            else if(w < 1.5)
+                result = -1;
+            else
+                result = 0;
+        }
+
+        public int NumberOfReflections()
+        {
+            if (result == -1)
+                return result;
+            else
+            {
+                int reflections = 0;
+                double x = h * b;
+                while (x > w) {
+                    if (x >= w)
+                    {
+                        reflections++;
+                        h = x;
+                    }
+                    x = h * b;
+                }
+                result = reflections;
+                return result;
+            }
+        } 
+
+
+    }
+}
---
+++ ZadanieTestowe/ZadanieTestowe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ZadanieTestowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Algorithm a = new Algorithm(10, 0.7, 1.5);
+            a.NumberOfReflections();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
